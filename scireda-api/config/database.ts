07384d4f7a8cafc9044badc89aab49cee4a70a33
Nathan Schneider <nathan.schneider4505@gmail.com>
2025-08-21 20:26:25 +0200

    Fix Fly.io and DB config

--- scireda-api/config/database.ts
+++ scireda-api/config/database.ts
@@ -1,16 +1,15 @@
 import env from '#start/env'
 import { defineConfig } from '@adonisjs/lucid'
-import fs from 'fs'
 
 const dbConfig = defineConfig({
-  connection: 'postgres',
+  connection: 'pg',
   connections: {
-    postgres: {
+    pg: {
       client: 'pg',
       connection: {
         connectionString: env.get('DATABASE_URL'),
         ssl: {
-          ca: fs.readFileSync('/app/certs/supabase-ca.crt').toString(),
+          rejectUnauthorized: false,
         },
       },
       migrations: {
