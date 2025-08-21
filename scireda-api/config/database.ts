ad34c9fc0ae39a8c2b793a00c7899568d3d6a084
Nathan Schneider <nathan.schneider4505@gmail.com>
2025-08-21 13:19:28 +0200

    Fix backend Fly.io deployment

--- scireda-api/config/database.ts
+++ scireda-api/config/database.ts
@@ -7,11 +7,10 @@ const dbConfig = defineConfig({
     postgres: {
       client: 'pg',
       connection: {
-        host: env.get('DB_HOST'),
-        port: env.get('DB_PORT'),
-        user: env.get('DB_USER'),
-        password: env.get('DB_PASSWORD'),
-        database: env.get('DB_DATABASE'),
+        connectionString: env.get('DATABASE_URL'),
+        ssl: {
+          rejectUnauthorized: false,
+        },
       },
       migrations: {
         naturalSort: true,
